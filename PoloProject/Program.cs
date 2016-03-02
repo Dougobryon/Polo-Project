4b5d2414cbeff8d50484d00463f18875d0ec96af
Your Name <you.example.com>
2016-03-02 10:58:37 -0500

    a.

---
+++ PoloProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoloProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { Console.WriteLine(The best part of cleveland is leaving);
+           
+        }
+    }
+}
